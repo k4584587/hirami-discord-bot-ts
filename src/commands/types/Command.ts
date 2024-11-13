f0ca3c1f34e4f923f94af7de732da50cd1c97abd
k4584587 <k4584587@naver.com>
2024-11-13 15:54:09 +0900

    Update .gitignore

--- src/commands/types/Command.ts
+++
@@ -1,6 +0,0 @@
-import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder, SlashCommandOptionsOnlyBuilder, ChatInputCommandInteraction } from 'discord.js';
-
-export interface Command {
-    data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder;
-    execute(interaction: ChatInputCommandInteraction): Promise<void>;
-}
--- src/events/index.ts
+++
@@ -1,9 +0,0 @@
-// src/events/index.ts
-import { Client } from 'discord.js';
-import { ready } from './ready';
-import { messageCreate } from './messageCreate';
-
-export const registerEvents = (client: Client): void => {
-    client.on('ready', ready);
-    client.on('messageCreate', messageCreate);
-};
